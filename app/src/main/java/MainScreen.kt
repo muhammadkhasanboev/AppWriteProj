daf1436f6e62ad49bc4886e95dcf4c7ab870f92a
Muhammad <muhammadxasanboyev1234@gmail.com>
2025-07-30 16:20:59 +0500

    MainScreen edited

--- app/src/main/java/MainScreen.kt
+++ app/src/main/java/MainScreen.kt
@@ -1,13 +1,21 @@
 package io.appwrite.starterkit
 
+import android.graphics.drawable.shapes.OvalShape
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.AlertDialogDefaults.containerColor
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -18,6 +26,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import io.appwrite.starterkit.io.appwrite.starterkit.data.NavItem
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 
 @Composable
@@ -29,9 +39,20 @@ fun MainScreen(modifier: Modifier = Modifier){
     )
     var selectedIndex by remember { mutableStateOf(0) }
     Scaffold(
-        modifier = modifier.fillMaxSize(),
+        modifier = modifier
+            .fillMaxSize()
+            .padding(start = 5.dp, end = 5.dp, bottom = 5.dp),
         bottomBar = {
-            NavigationBar {
+            Surface(
+                shape = RoundedCornerShape(percent = 90),
+                tonalElevation = 8.dp,
+                shadowElevation = 10.dp,
+                modifier = Modifier.height(100.dp)
+            ) {
+            NavigationBar(
+                containerColor = Color(0xFFF5BA69),
+
+            ) {
                 navItemList.forEachIndexed { index, navItem ->
                     NavigationBarItem(
                         selected = selectedIndex == index,
@@ -46,9 +67,18 @@ fun MainScreen(modifier: Modifier = Modifier){
                             Text(
                                 text = navItem.label
                             )
-                        }
+                        },
+                        colors = NavigationBarItemDefaults.colors(
+                            selectedIconColor = Color(0xFFF5BA69),
+                            selectedTextColor = Color.White,
+                            indicatorColor = Color.White,
+                            unselectedIconColor = Color.Black,
+                            unselectedTextColor = Color.Black
+
+                        )
                         )
                 }
+            }
             }
         }
     ) {
