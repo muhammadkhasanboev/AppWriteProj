5bba4845125804f622226f03f3d2231b3efca46a
Muhammad <muhammadxasanboyev1234@gmail.com>
2025-07-31 11:09:30 +0500

    customize page icons and texts added

---
+++ app/src/main/java/io/appwrite/starterkit/screens/CustomizePage.kt
@@ -0,0 +1,98 @@
+package io.appwrite.starterkit.io.appwrite.starterkit.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.ui.res.painterResource
+import io.appwrite.starterkit.R
+
+
+data class SubjectItem(
+    val name: String,
+    val categoryId: Int,
+    val iconRes: Int)
+
+
+@Composable
+fun CustomizePage(modifier: Modifier = Modifier){
+   val itemsList = listOf(
+       SubjectItem("Math", 1, R.drawable.math),
+       SubjectItem("Science & Nature", 2, R.drawable.science),
+       SubjectItem("Animals", 3, R.drawable.animals),
+       SubjectItem("General Knowledge", 4, R.drawable.general),
+       SubjectItem("Books", 5, R.drawable.books),
+       SubjectItem("Geography", 6, R.drawable.geography),
+       SubjectItem("Art", 7, R.drawable.art),
+       SubjectItem("Video Games", 8, R.drawable.gaming),
+       SubjectItem("Sports", 9, R.drawable.sports),
+       SubjectItem("Computers", 10, R.drawable.computer),
+       SubjectItem("History", 11, R.drawable.history),
+       SubjectItem("Vehicles", 12, R.drawable.vehicles)
+   )
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ){
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            modifier = Modifier.fillMaxSize().fillMaxWidth(),
+            state = rememberLazyGridState(),
+            contentPadding = PaddingValues(16.dp),
+            userScrollEnabled = true
+        ) {
+            items(itemsList) { item ->
+                SubjectCard(subject = item, onClick = {})
+            }
+
+        }
+}}
+
+@Composable
+fun SubjectCard(subject: SubjectItem,
+                onClick: (SubjectItem)->Unit) {
+    Column(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+            .clickable {onClick(subject) }
+            .background(Color(0xFFE0F7FA))
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = subject.iconRes),
+            contentDescription = subject.name,
+            modifier = Modifier.size(64.dp)
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(text = subject.name, style = MaterialTheme.typography.bodyLarge)
+    }
+}
+
+@Composable
+@Preview(showSystemUi = true)
+fun customizePagePreview(){
+    CustomizePage()
+
+}
